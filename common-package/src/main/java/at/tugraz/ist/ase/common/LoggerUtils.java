e07018145df24f41a1709d615f7c94b42e34f2d9
Lê Viết Mẫn <manleviet@gmail.com>
2022-05-31 12:14:49 +0200

    New package 1.3.9-alpha-15

--- common-package/src/main/java/at/tugraz/ist/ase/common/LoggerUtils.java
+++ common-package/src/main/java/at/tugraz/ist/ase/common/LoggerUtils.java
@@ -6,7 +6,9 @@
  * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
  */
 
-package at.tugraz.ist.ase.common;import lombok.Getter;
+package at.tugraz.ist.ase.common;
+
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
